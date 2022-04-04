5e6207c4669f67b21ede53f80d72fb3d67e3708e
unknown <taha.iu.bd@gmail.com>
2022-04-04 16:02:46 +0600

    deploy 404 solved

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -4,8 +4,8 @@ import CustomLink from '../CustomLink/CustomLink'
 
 const Nav = () => {
     return (
-        <header className="text-gray-600 body-font md:sticky top-0 z-50 ">
-            <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+        <header className="text-gray-600 bg-white  body-font md:sticky top-0 z-50 ">
+            <div className="container mx-auto  flex flex-wrap p-5 flex-col md:flex-row items-center">
                 <Link
                     to="/"
                     className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0"
