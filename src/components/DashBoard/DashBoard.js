7d46697fa3d116de7a00a6f134718f8b17750541
unknown <taha.iu.bd@gmail.com>
2022-04-05 14:10:07 +0600

    done

--- src/components/DashBoard/DashBoard.js
+++ src/components/DashBoard/DashBoard.js
@@ -6,51 +6,53 @@ import TwoLevelPieChart from '../TwoLevelPieChart/TwoLevelPieChart'
 
 const DashBoard = () => {
     return (
-        <div className="md:flex flex-wrap justify-center items-center gap-12">
-            <div className="m-6 ">
-                <StackedAreaChart />
-                <h1
-                    className="text-gray-500 text-3xl text-center
+        <div className="container mx-auto">
+            <div className="md:flex flex-wrap  justify-center items-center gap-12">
+                <div className="m-6 ">
+                    <StackedAreaChart />
+                    <h1
+                        className="text-gray-500 text-3xl text-center
                  title-font font-medium pt-10"
-                >
-                    Stacked Area Chart
-                </h1>
-            </div>
-            <div className="m-6">
-                <ThreeLevelPieChart />
-                <h1
-                    className="text-gray-500 text-3xl text-center
+                    >
+                        Stacked Area Chart
+                    </h1>
+                </div>
+                <div className="m-6">
+                    <ThreeLevelPieChart />
+                    <h1
+                        className="text-gray-500 text-3xl text-center
                  title-font font-medium mb-1"
-                >
-                    Three Level PieChart
-                </h1>
-            </div>
-            <div className="m-6 ">
-                <ChartBar />
-                <h1
-                    className="text-gray-500 text-3xl text-center
+                    >
+                        Three Level PieChart
+                    </h1>
+                </div>
+                <div className="m-6 ">
+                    <ChartBar />
+                    <h1
+                        className="text-gray-500 text-3xl text-center
                  title-font font-medium pt-10"
-                >
-                    Mix Bar Chart
-                </h1>
-            </div>
-            <div className="m-6">
-                <TwoLevelPieChart></TwoLevelPieChart>
-                <h1
-                    className="text-gray-500 text-3xl text-center
+                    >
+                        Mix Bar Chart
+                    </h1>
+                </div>
+                <div className="m-6">
+                    <TwoLevelPieChart></TwoLevelPieChart>
+                    <h1
+                        className="text-gray-500 text-3xl text-center
                  title-font font-medium mb-1"
-                >
-                    Two Level PieChart
-                </h1>
-            </div>
-            <div className="m-6 hidden md:block">
-                <SpecifiedDomainRadarChart></SpecifiedDomainRadarChart>
-                <h1
-                    className="text-gray-500 text-3xl text-center
+                    >
+                        Two Level PieChart
+                    </h1>
+                </div>
+                <div className="m-6 hidden md:block">
+                    <SpecifiedDomainRadarChart></SpecifiedDomainRadarChart>
+                    <h1
+                        className="text-gray-500 text-3xl text-center
                  title-font font-medium mb-1"
-                >
-                    Specified Domain Radar Chart
-                </h1>
+                    >
+                        Specified Domain Radar Chart
+                    </h1>
+                </div>
             </div>
         </div>
     )
