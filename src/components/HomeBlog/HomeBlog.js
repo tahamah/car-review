b09ba61f8965bc62d17ab1f7786afb9e4d906c2a
unknown <taha.iu.bd@gmail.com>
2022-08-15 15:36:09 +0600

    change-origin

--- src/components/HomeBlog/HomeBlog.js
+++ src/components/HomeBlog/HomeBlog.js
@@ -1,4 +1,28 @@
+import { useState } from 'react'
+
 const HomeBlog = () => {
+    const [img, setImg] = useState(
+        'https://api.ferrari.com/cms/network/medias//resize/6245832fad8c7d2af3d9fb64?apikey=9QscUiwr5n0NhOuQb463QEKghPrVlpaF&width=530&height=597'
+    )
+    const [price, setPrice] = useState('335,000')
+    const HandleImg = (color) => {
+        if (color === 'red') {
+            setImg(
+                'https://api.ferrari.com/cms/network/medias//resize/6245832fad8c7d2af3d9fb64?apikey=9QscUiwr5n0NhOuQb463QEKghPrVlpaF&width=530&height=597'
+            )
+            setPrice('335,000')
+        } else if (color === 'white') {
+            setImg(
+                'https://images.unsplash.com/photo-1531436062681-6e2a24161410?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+            )
+            setPrice('336,500')
+        } else if (color === 'blue') {
+            setImg(
+                'https://images.unsplash.com/photo-1545061371-98a8355c9c0d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80'
+            )
+            setPrice('334,000')
+        }
+    }
     return (
         <section className="text-gray-600 body-font overflow-hidden">
             <div className="container px-5 py-24 mx-auto">
@@ -6,7 +30,7 @@ const HomeBlog = () => {
                     <img
                         alt="ecommerce"
                         className="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded"
-                        src="https://api.ferrari.com/cms/network/medias//resize/6245832fad8c7d2af3d9fb64?apikey=9QscUiwr5n0NhOuQb463QEKghPrVlpaF&width=530&height=597"
+                        src={img}
                     />
                     <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
                         <h2 className="text-sm title-font text-gray-500 tracking-widest">
@@ -128,14 +152,23 @@ const HomeBlog = () => {
                         <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-100 mb-5">
                             <div className="flex">
                                 <span className="mr-3">Color</span>
-                                <button className="border-2 border-gray-300 rounded-full w-6 h-6 focus:outline-none"></button>
-                                <button className="border-2 border-gray-300 ml-1 bg-red-600 rounded-full w-6 h-6 focus:outline-none"></button>
-                                <button className="border-2 border-gray-300 ml-1 bg-blue-500 rounded-full w-6 h-6 focus:outline-none"></button>
+                                <button
+                                    onClick={() => HandleImg('white')}
+                                    className="border-2 border-gray-300 rounded-full w-6 h-6 focus:outline-none"
+                                ></button>
+                                <button
+                                    onClick={() => HandleImg('red')}
+                                    className="border-2 border-gray-300 ml-1 bg-red-600 rounded-full w-6 h-6 focus:outline-none"
+                                ></button>
+                                <button
+                                    onClick={() => HandleImg('blue')}
+                                    className="border-2 border-gray-300 ml-1 bg-blue-500 rounded-full w-6 h-6 focus:outline-none"
+                                ></button>
                             </div>
                         </div>
                         <div className="flex">
                             <span className="title-font font-medium text-2xl text-gray-900">
-                                $ 335,000
+                                $ {price}
                             </span>
                             <button className="flex ml-auto text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded">
                                 <span className="mr-3">Buy Now</span>
